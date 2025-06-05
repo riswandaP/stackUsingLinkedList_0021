50d0550c6c03c7ad98f9608d3cebf7f83e0a2d3b
riswandaP <riswandaputra03@gmail.com>
2025-06-05 11:16:30 +0700

    Membuat case opsi 1

--- stack.cpp
+++ stack.cpp
@@ -88,5 +88,12 @@ int main()
         cout << "Enter your choice: ";
         cin >> choice;
 
+        switch (choice)
+        {
+            case 1: 
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
 
 }
